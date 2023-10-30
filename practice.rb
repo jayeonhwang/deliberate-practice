9e1f02ddd99581be42353a887f83a83fe696e7c6
Jayeon Hwang <jayeon512@gmail.com>
2023-10-30 11:18:36 -0500

    Add loop example

--- practice.rb
+++ practice.rb
@@ -1,4 +1,12 @@
 #Write a program that uses a variable to store a word, then prints that word with all capital letters.
-
 word = "hello world"
 puts word.upcase
+
+#Write a while loop that prints the word "hello" 5 times.
+
+index = 0
+while index < 5
+    puts "hello"
+ index = index + 1
+end
+
