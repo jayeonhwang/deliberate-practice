4169a97be83cd084e54108c67ab9db8a6416d5fc
Jayeon Hwang <jayeon512@gmail.com>
2023-11-30 10:53:41 -0600

    Create 1130.rb

---
+++ 1130.rb
@@ -0,0 +1,84 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 1
+puts number + 10
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+word = "merry christmas"
+puts word.upcase
+
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+number1 = 1
+number2 = 3
+
+puts number1 + number2
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+word = "hello"
+puts word.reverse
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 1 
+puts number * 10
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "hey"
+word2 = "Hi"
+
+puts (word1 + word2).upcase
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "apple"
+puts = word.length
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+number = 1.99
+
+puts number.to_i
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+number1 = 3
+number2 = 10
+
+puts number1 * number2
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+word = "BANANA"
+
+puts word.downcase
+
+
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+
+# Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+
+# Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
