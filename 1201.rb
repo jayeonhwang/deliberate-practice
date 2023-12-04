1f5c919383ce2832069cadf4506ce6bd5c3d34c0
Jayeon Hwang <jayeon512@gmail.com>
2023-12-04 15:23:05 -0600

    Create 1201.rb

---
+++ 1201.rb
@@ -0,0 +1,57 @@
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+ number = 9
+
+ if number == 10
+   puts 0
+ else 
+   puts -1
+ end
+
+# Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+number = 7
+
+if number < 10 
+  puts -1
+elsif number > 10
+  puts 1
+elsif number == 10
+  puts 0
+end
+
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+number1 = 9
+number2 = 17
+
+if number1 < 10 && number2 < 10
+  puts 1
+else
+  puts 0
+end
+
+# Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+number = 10000
+if number > 9000
+  puts 1
+else 
+  puts -1
+end
+
+# Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+
+# Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+
+# Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+
+# Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
