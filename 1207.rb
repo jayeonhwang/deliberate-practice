d544e003202d31737c07ac7c1f5b815f45d55522
Jayeon Hwang <jayeon512@gmail.com>
2023-12-08 08:48:26 -0600

    Update 1207.rb

--- 1207.rb
+++ 1207.rb
@@ -24,3 +24,4 @@ p a
 # Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
 # Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
