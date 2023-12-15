b2d8e2451047fd4521466088863cdd364158b1eb
Jayeon Hwang <jayeon512@gmail.com>
2023-12-15 10:24:40 -0600

    Create 1215.js

---
+++ 1215.js
@@ -0,0 +1,46 @@
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+var number = 1;
+console.log(number);
+
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+var word = "hi"
+console.log(word);
+
+// Write a program that uses variables to store two numbers, then prints the numbers added together.
+var n1 = 2;
+var n2 = 3;
+console.log(n1 + n2);
+
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+var something = "onepiece";
+console.log(something.split("").reverse().join(""));
+
+// Write a program that uses a variable to store a number, then prints the number times 10.
+var n3 = 10;
+console.log(n3 * 10);
+
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+var w1 = "cat";
+var w2 = "dog";
+console.log(w1.toLocaleUpperCase() + w2.toLocaleUpperCase());
+
+// Write a program that uses a variable to store a word, then prints the number of letters in the word.
+var w3 = "rabbits";
+console.log(w3.length);
+// Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+var n4 = 76.89;
+console.log(parseInt(n4));
+
+// Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+var n5 = 77;
+var n6 = 89;
+console.log(n5 * n6);
+// Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+var w4 = "Bird";
+var w5 = "PIG";
+
+console.log(w4.toLocaleLowerCase() + w5.toLocaleLowerCase());
+
