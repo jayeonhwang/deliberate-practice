dd78199004493dd5af54163f67c492b100041386
Jayeon Hwang <jayeon512@gmail.com>
2023-12-15 10:08:01 -0600

    Create 1211.rb

---
+++ 1211.rb
@@ -0,0 +1,24 @@
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+info = {"first_name" => "jay", "last_name" => "hwang", "email" =>"yean@gmail.com" }
+p info["first_name"]
+p info["last_name"]
+p info ["email"]
+
+# Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+# Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+# Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+# Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+# Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+# Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+# Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+# Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+# Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+# Python solution
