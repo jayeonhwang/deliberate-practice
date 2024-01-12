bdd257fa7499c7e665af19c650194abf135b55b7
Jayeon Hwang <jayeon512@gmail.com>
2024-01-12 16:03:45 -0600

    Update 1226.js

--- 1226.js
+++ 1226.js
@@ -1,7 +1,7 @@
 //Convert an array of arrays into a hash.
 // For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
-const pairs = [[1, 3], [8, 9], [2, 16]];
+// const pairs = [[1, 3], [8, 9], [2, 16]];
 // const pairsHash = {};
 // let index = 0;
 
@@ -64,26 +64,51 @@ const pairs = [[1, 3], [8, 9], [2, 16]];
 // Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 // For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
 
-let people = {321:{name: "Alice", age: 31}, 322: {name: "Maria", age: 27}};
-let arrayPeople = [];
+// let people = {321:{name: "Alice", age: 31}, 322: {name: "Maria", age: 27}};
+// let arrayPeople = [];
 
-Object.entries(people).forEach(function([id, info]) {
-  info.id = parseInt(id);
-  arrayPeople.push(info);
-});
+// Object.entries(people).forEach(function([id, info]) {
+//   info.id = parseInt(id);
+//   arrayPeople.push(info);
+// });
 
-console.log(arrayPeople);
+// console.log(arrayPeople);
 
-// Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
+// 6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 // For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
 
+// let words = ["do", "or", "do", "not"];
+// let wordFrequencies = {};
+// let index = 0;
+
+// while (index < words.length) {
+//   let word = words[index];
+//   if (wordFrequencies[word] === undefined) {
+//     wordFrequencies[word] = 0;
+//   }
+//   wordFrequencies[word] += 1;
+//   index++;
+// }
+// console.log(wordFrequencies);
 
 // Convert a hash into a flat array containing all the hash’s keys and values.
 // For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
 
+var letters = {"a" :1, "b": 2, "c" : 3, "d" : 4};
+var letterArray = [];
+
+Object.entries(letters).forEach(function([key,value]) {
+  letterArray.push(key);
+  letterArray.push(value);
+});
+
+console.log(letterArray);
+
+
 
 // Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
 // For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+
 
 
 // Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
