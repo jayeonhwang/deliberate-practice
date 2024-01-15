3cebcfbefec72772744ae1b08d84b220eb8a6605
Jayeon Hwang <jayeon512@gmail.com>
2024-01-15 07:11:51 -0600

    jan-15 deliberate

---
+++ 0115.js
@@ -0,0 +1,87 @@
+// Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+// let fruits = ['apple', 'banana', 'peach'];
+
+// fruits.push("fig");
+// fruits.push("orange");
+
+// console.log(fruits);
+
+
+// Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+// let letters = ["a", "b", "c"];
+// letters[1] = 3;
+
+// console.log(letters);
+
+// Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+// let numbers = [1,2,3,4,5];
+// let index = 0;
+// while (index < numbers.length) {
+//   console.log(numbers[index]);
+//   index += 1;
+// }
+
+
+// Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+// let numbers = [1];
+
+// numbers.push(3);
+// numbers.push(5);
+// numbers.push(7);
+
+// console.log(numbers);
+
+
+
+// Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+// let words = ["hello", "pencil", "rose"];
+// words[2] = words[2].toUpperCase();
+// console.log(words);
+
+// Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+// let name = ["mia", "sarah", "jane"];
+
+// let index = 0;
+// while (index < name.length) {
+//   console.log(name[index]);
+//   index += 1;
+// }
+
+// Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+// let strings = ["daisy", "rose", "lily"];
+
+// strings.push("lavender");
+// console.log(strings);
+
+
+// Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+// let numbers = [1,2,3,4,5];
+// numbers[0] = numbers[0] * 10;
+
+// console.log(numbers);
+
+
+// Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+let numbers = [5, 6];
+let index = 0;
+
+while (index < numbers.length) {
+  console.log(numbers[index]);
+  index += 1;
+}
+
+
+// Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+let contries = ["USA", "france", "Japan"];
+contries.push("Korea");
+console.log(contries);
