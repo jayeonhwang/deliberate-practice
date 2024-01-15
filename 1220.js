1a83e3622842374a7e378e2993a788d2fc60f986
Jayeon Hwang <jayeon512@gmail.com>
2024-01-14 19:58:39 -0600

    js practice

--- 1220.js
+++ 1220.js
@@ -1,20 +1,87 @@
+'use strict';
+
 // Write a while loop to print the numbers 1 through 10.
+
+// let index = 1;
+
+// while (index <= 10) {
+//   console.log(index);
+//   index += 1;
+// }
+
 
 // Write a while loop that prints the word "hello" 5 times.
 
+// let index = 0;
+// while (index < 5) {
+//   console.log("hello");
+//   index += 1;
+// }
+
+
 // Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
+// while (true) {
+//   let input = window.prompt("Enter a word: ");
+//   if (input === 'stop') {
+//     break;
+//   }
 // Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+// let index = 0;
+// while (index < 100) {
+//   console.log(index);
+//   index += 5;
+// }
 
 // Write a while loop that prints the number 9000 ten times.
 
+// let index = 0;
+// while (index < 10) {
+//   console.log(9000);
+//   index += 1;
+// }
+
 // Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// while (true) {
+//   let input = window.prompt("Please enter a numbe: ");
+//   if (input > 10 ) {
+//     break;
+//   }
+// }
 
 // Write a while loop that prints the numbers 50 to 70.
 
+// let index = 50;
+// while (index <= 70) {
+//   console.log(index);
+//   index += 1;
+// }
+
 // Write a while loop that prints the phrase "Around the world" 144 times.
+
+// let index = 0;
+// while (index < 144) {
+//   console.log("Around the World");
+//   index += 1;
+// }
 
 // Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
+// while(true) {
+//   let input = window.prompt("Please enter a long word :");
+//   if (input.length > 5) {
+//     break;
+//   }
+// }
+
+
+
 // Write a while loop that prints the even numbers from 2 to 40.
 
+let index = 2;
+
+while (index <= 40) {
+  console.log(index);
+  index += 2;
+}
