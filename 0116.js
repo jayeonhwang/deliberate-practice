69f8e51bd902b6881b3078ead6f6f05bfbae658b
Jayeon Hwang <jayeon512@gmail.com>
2024-01-16 07:36:00 -0600

    0116

---
+++ 0116.js
@@ -0,0 +1,74 @@
+// Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+// let person = {firstName: "jay", lastName:"Hwang", email:"jayeon512@gmail.com"};
+
+// console.log(person.firstName);
+// console.log(person["lastName"]);
+// console.log(person.email);
+
+
+// Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+
+// let people = [{firstName:"jei", lastName:"h"},{firstName:"young", lastName:"Kim"},{firstName:"elli", lastName:"smith"}];
+
+// console.log(people[0]);
+
+
+
+// Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+let prices = {tea: 3, coffee: 3 , latte: 5 ,};
+prices["americano"] = 4;
+console.log(prices);
+
+
+// Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+let book = { title: "super habit", author:"michael Madsen", pages:"150"};
+
+console.log(book.title);
+console.log(book.author);
+console.log(book.pages);
+
+// Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+let books = [{title:"Pachinko", author:"Min Jin Lee"},{title:"super habit", author:"michael Madsen"},{title:"Project Hail Mary", author:"Andy Weir"}];
+
+console.log(books[2].author);
+
+// Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the
+
+let states = {Chicago:"Springfield", Newyork:"Newyork",california:"Sacramento"};
+
+states["Minnesota"] = "Saint Paul";
+console.log(states);
+
+// Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+let laptop = {brand:"apple", model: "air book m2", year: 2022};
+
+console.log(laptop.brand);
+console.log(laptop.model);
+console.log(laptop.year);
+
+
+// Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+let laptops = [{brand:"apple", model: "air book m2", year: 2022},{brand:"LG", model: "gram", year: 2020},{brand:"samsung", model: "galaxy", year: 2018}];
+
+console.log(laptops[2].model);
+
+// Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+let words = {cat:"cute animal", dog:"adorable animal"};
+words["rabbit"] = "cute animal has long ears";
+
+console.log(words);
+
+// Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+let shirt = {brand:"polo", color: "pink", size: "M"};
+console.log(shirt.brand);
+console.log(shirt.color);
+console.log(shirt.size);
