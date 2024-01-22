f5b992e2ad975cd45b9ca26cb7de5437381acbdf
Jayeon Hwang <jayeon512@gmail.com>
2024-01-22 06:47:00 -0600

    0118

---
+++ 0118.js
@@ -0,0 +1,51 @@
+// 1.Write a Song class with attributes for name, artist, and duration.
+ 
+class Song {
+  constructor(name, artist, duration) {
+    this.name = name;
+    this.artist = artist;
+    this.duration = duration;
+  }
+}
+let song = new Song("Gloria", "Patti Smith", "5:56");
+console.log(song);
+
+
+// 2.Write a Rectangle class with attributes for width and height.
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+let nemo = new Rectangle(5, 7);
+console.log(nemo);
+
+
+// 3.Write a Person class with attributes for name and age.
+
+Clas
+
+
+// 4.Write a Location class with attributes for latitude and longitude.
+
+
+// 5.Write an Account class with attributes for name and balance.
+
+
+// 6.Write a Movie class with attributes for title, director, and year.
+
+
+// 7.Write a Car class with attributes for make, model, year, and color.
+
+
+// 8.Write a Point class with attributes for x, y, and z coordinates.
+
+
+// 9.Write a Book class with attributes for title, author, and year.
+
+
+// 10.Write a Plant class with attributes for name, size, and price.
+
