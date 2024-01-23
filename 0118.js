3e696509175f55d8dfd5922fd22003ac127f7b7a
Jayeon Hwang <jayeon512@gmail.com>
2024-01-23 09:07:39 -0600

    0118 update

--- 0118.js
+++ 0118.js
@@ -26,26 +26,105 @@ console.log(nemo);
 
 // 3.Write a Person class with attributes for name and age.
 
-Clas
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let person = new Person("J", 33);
+console.log(person);
 
 
 // 4.Write a Location class with attributes for latitude and longitude.
 
+class Location {
+  constructor (latitude , longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+}
+
+let chicago = new Location(41.8781, 87.6298);
+console.log(chicago);
+
 
 // 5.Write an Account class with attributes for name and balance.
+class Account {
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = balance;
+  }
+}
+
+let account = new Account ("K", "500000");
+console.log(account);
 
 
 // 6.Write a Movie class with attributes for title, director, and year.
 
+class Movie {
+  constructor( title, director, year) {
+    this.title = title;
+    this.director = director;
+    this.year = year;
+  }
+}
+
+let movie = new Movie("Moana", "Ron Clements", 2016 );
+
+console.log(movie);
 
 // 7.Write a Car class with attributes for make, model, year, and color.
 
+class Car {
+  constructor(make, model, year, color) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+}
+
+let car = new Car("Totyota", "corolla", 2018, "White");
+console.log(car);
 
 // 8.Write a Point class with attributes for x, y, and z coordinates.
 
+class Point {
+  constructor(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+
+let point = new Point(5,6,7);
+console.log(point);
 
 // 9.Write a Book class with attributes for title, author, and year.
 
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+}
+
+let book = new Book("The Vegetarian", "Han Kang", 2007);
+console.log(book);
 
 // 10.Write a Plant class with attributes for name, size, and price.
 
+class Plant {
+  constructor(name, size, price) {
+    this.name = name;
+    this.size = size;
+    this.price = price;
+  }
+}
+
+let plant = new Plant("Olive tree", "30 inch", 70);
+console.log(plant);
