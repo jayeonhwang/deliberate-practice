a0cf6dd8559fbf7dd90ad14f28f41f60f1da9557
Jayeon Hwang <jayeon512@gmail.com>
2024-01-24 08:13:08 -0600

    0123 update

---
+++ 0123.js
@@ -0,0 +1,61 @@
+// Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+// let first_name = "jay";
+// let last_name = "hwang";
+
+// console.log(first_name+" "+last_name);
+
+
+// Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+let first_name = "jay";
+let last_name = "hwang";
+
+console.log(`${first_name} ${last_name}`);
+
+
+// Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// let word = window.prompt("enter a word: ");
+// if (word === 'marco') {
+//   console.log("polo");
+// }
+
+// Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+// let c1 = 'yellow';
+// let c2 = 'green';
+// let c3 = 'red';
+
+// console.log(c1 + c2 + c3);
+
+// Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+let c1 = 'yellow';
+let c2 = 'green';
+let c3 = 'red';
+
+console.log(`I like ${c1}, ${c2} and ${c3}`);
+
+
+
+// Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+let name = window.prompt(`please enter your name: `);
+if (name === "santa") {
+  console.log("you're not santa");
+}
+
+
+// Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+
+
+// Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+
+// Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+
+// Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
