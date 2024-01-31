08bf3eb2b31a417d4ac047fe0d23ac3cdf21de85
Jayeon Hwang <jayeon512@gmail.com>
2024-01-31 08:03:51 -0600

    Update 0129.js

--- 0129.js
+++ 0129.js
@@ -63,18 +63,61 @@ console.log(totalLength);
 // Start with an array of hashes and find the hash with the lowest price (from the :price key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
+let lowPrice = items[0];
+
+items.forEach(function (item) {
+  if (item.price < lowPrice.price) {
+    lowPrice = item;
+  }
+});
+
+console.log(lowPrice);
+
+
 // Start with an array of numbers and compute product of all the numbers.
 // For example, [5, 10, 8, 3] becomes 1200.
+
+let n = [5, 10, 8, 3];
+let products = n.reduce(function (product, number) {
+  return product * number;
+}, 1);
+
+console.log(products);
+
+
 
 
 // Start with an array of strings and combine them all into a single string, separated by dashes.
 // For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
+let singleString = sports.reduce(function (total, sport) {
+  return total + sport + "-";
+}, "-");
+
+console.log(singleString);
 
 // Start with an array of hashes and find the hash with the shortest name (from the :name key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+let shortName = items[0];
+items.forEach(function (item) {
+  if (item.name.length < shortName.name.length) {
+    shortName = item;
+  }
+});
+
+console.log(shortName);
 
 
 // Start with an array of numbers and compute the maximum number.
 // For example, [5, 10, 8, 3] becomes 10.
 
+let maximum = n.reduce(function (max, number) {
+  if (max < number) {
+    return number;
+  } else {
+    return max;
+  }
+}, n[0]);
+
+console.log(maximum);
